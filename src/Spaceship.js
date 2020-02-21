55ddce142bc73d636d08320621be9a7b790612b9
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-02-21 15:38:39 +0100

    .

--- src/Spaceship.js
+++ src/Spaceship.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Spaceship() {
   return (
     <div>
-      <h1>My Spaceship</h1>
+      <h1>My Spaceship!!!</h1>
       <div>Cockpit</div>
       <div>Engine</div>
     </div>
