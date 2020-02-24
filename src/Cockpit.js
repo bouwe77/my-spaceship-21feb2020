246539cde4b83f5ac6d868637f4c608a837641c6
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2020-02-24 10:38:58 +0100

    .

--- src/Cockpit.js
+++ src/Cockpit.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useServer from "./server/useServer";
+import { getSpaceObjects } from "./server/api";
 
 export default function Cockpit({ engineMode, setEngineMode }) {
   const [setCourse] = useServer("Minotuar");
+  const [spaceObjects, setSpaceObjects] = useState([]);
+  const [selectedSpaceObjectName, setSelectedSpaceObjectName] = useState();
+  const [destinationX, setDestinationX] = useState();
+  const [destinationY, setDestinationY] = useState();
 
   const IDLE = "idle";
   const THRUSTERS = "thrusters";
   const IMPULSE = "impulse";
 
   function go() {
-    setCourse({ x: 333, y: 444 }, 1);
+    setCourse({ x: destinationX, y: destinationY }, 100);
   }
+
+  useEffect(() => {
+    async function fetch() {
+      const result = await getSpaceObjects();
+      setSpaceObjects(result);
+    }
+    fetch();
+  }, []);
+
+  function handleSpaceObjectChanged(event) {
+    // Determine the value of the selected item, which is the space object name.
+    const selectedSpaceObjectName = event.target.value;
+    setSelectedSpaceObjectName(selectedSpaceObjectName);
+
+    // If nothing was selected, quit further processing.
+    if (!selectedSpaceObjectName) return;
+
+    // A space object was selected, find it in the spaceObjects array.
+    const selectedSpaceObject = spaceObjects.find(s => s.name === selectedSpaceObjectName);
+
+    // Update the destination according to the selected space object's destination.
+    setDestinationX(selectedSpaceObject.destinationX);
+    setDestinationY(selectedSpaceObject.destinationY);
+  }
+
   return (
     <div className="cockpit">
       <div className="engineModePanel">
@@ -47,29 +77,22 @@ export default function Cockpit({ engineMode, setEngineMode }) {
         </div>
       </div>
       <div className="navigationPanel">
+        <div>
+          <select value={selectedSpaceObjectName} onChange={handleSpaceObjectChanged}>
+            <option key="-" value="" />
+            {spaceObjects.map(spaceObject => (
+              <option key={spaceObject.name} value={spaceObject.name}>
+                {spaceObject.name} ({spaceObject.type})
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          {destinationX},{destinationY}
+        </div>
         <button onClick={go}>Go</button>
       </div>
       <div className="locationPanel" />
     </div>
   );
 }
-
-/*
-  function handleSpaceObjectChanged(event) {
-    // Determine the value of the selected item, which is the space object name.
-    const selectedSpaceObjectName = event.target.value;
-    setSelectedSpaceObjectName(selectedSpaceObjectName);
-
-    // If nothing was selected, quit further processing.
-    if (!selectedSpaceObjectName) return;
-
-    // A space object was selected, find it in the spaceObjects array.
-    const selectedSpaceObject = spaceObjects.find(
-      s => s.name === selectedSpaceObjectName
-    );
-
-    // Update the destination according to the selected space object's destination.
-    setDestinationX(selectedSpaceObject.destinationX);
-    setDestinationY(selectedSpaceObject.destinationY);
-  }
-  */
